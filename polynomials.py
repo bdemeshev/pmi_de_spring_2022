43e5d244714af8c5cd7e89d914a0052eaec1585c
bdemeshev <boris.demeshev@gmail.com>
2022-02-08 18:00:08 +0300

    class 3, 4

---
+++ polynomials.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  8 10:21:37 2022
+
+@author: boris
+"""
+
+from sympy import *
+x = symbols('x')
+
+expand(((x - 3)** 2 + 4) * (x-2) ** 2)
+
+expand(((x - 3)** 2 + 4) * (x-2))
+
+expand(((x - 3)** 2 + 4) )
+
